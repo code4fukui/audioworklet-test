3c99492f624e17861ce5c6b02504ab018a85e5fd
Taisuke Fukuno <fukuno@jig.jp>
2024-01-21 08:29:59 +0900

    add beep

---
+++ BeepNode.js
@@ -0,0 +1,15 @@
+export class BeepNode extends AudioWorkletNode {
+  static async create(context) {
+    await context.audioWorklet.addModule("beep-processor.js");
+    return new BeepNode(context);
+  }
+  constructor(context) {
+    super(context, "beep-processor");
+  }
+  setVolume(vol) {
+    this.port.postMessage({ vol });
+  }
+  setFreq(freq) {
+    this.port.postMessage({ freq });
+  }
+};
