ebb02b9429bef8d1d850090962f894e9e7709b42
Taisuke Fukuno <fukuno@jig.jp>
2024-01-22 08:10:14 +0900

    improve performance

--- BeepNode.js
+++ BeepNode.js
@@ -5,14 +5,23 @@ export class BeepNode extends AudioWorkletNode {
   }
   constructor(context) {
     super(context, "beep-processor");
+    this.freq = -1;
+    this.vol = -1;
   }
   setVolume(vol) {
+    if (vol == this.vol) return;
+    this.vol = vol;
     this.port.postMessage({ vol });
   }
   setFreq(freq) {
+    if (freq == this.freq) return;
+    this.freq = freq;
     this.port.postMessage({ freq });
   }
   setFreqVolume(freq, vol) {
+    if (vol == this.vol && freq == this.freq) return;
+    this.vol = vol;
+    this.freq = freq;
     this.port.postMessage({ freq, vol });
   }
 };
--- beep-processor.js
+++ beep-processor.js
@@ -2,19 +2,22 @@ class BeepProcessor extends AudioWorkletProcessor {
   constructor() {
     super();
     this.vol = 0;
-    this.period = sampleRate / 440 / 2;
+    this.period = 0;
     this.cnt = 0;
-    this.state = 1;
+    this.state = 0;
     this.port.onmessage = e => {
       if (e.data.vol) {
         this.vol = e.data.vol;
       }
       if (e.data.freq) {
         this.period = sampleRate / e.data.freq / 2;
+      } else if (e.data.freq == 0) {
+        this.period = 0;
       }
     };
   }
   process(inputs, outputs, parameters) {
+    if (!this.period) return true;
     const output = outputs[0];
     const chlen = output.length;
     const len = output[0].length;
@@ -23,7 +26,7 @@ class BeepProcessor extends AudioWorkletProcessor {
       for (let j = 0; j < chlen; j++) {
         output[j][i] = vol;
       }
-      if (this.cnt++ > this.period) {
+      if (this.period && this.cnt++ > this.period) {
         this.state = 1 - this.state;
         this.cnt = 0;
       }
